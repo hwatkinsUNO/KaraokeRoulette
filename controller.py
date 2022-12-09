3dbb2ae09fdcba589cf3c6724ea26736f6a783f6
hwatkinsUNO <118573827+hwatkinsUNO@users.noreply.github.com>
2022-12-09 17:29:37 -0600

    Slider controls wip

--- controller.py
+++ controller.py
@@ -36,13 +36,68 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.pushButton_Random.clicked.connect(lambda: self.random())
-
+        self.pushButton_Song.clicked.connect(lambda: self.output())
 
     def random(self):
         i = random.randint(0, 515)
         self.label_Song.setText(f'"{title_list[i]}"\n{artist_list[i]}')
 
     def output(self):
-    #     print(
-    #         f'"{title_list[i]} by {artist_list[i]} ({year_list[i]})\nGenre:{genre_list[i]} Time Period:{time_period_list[i]}\n'
-    #         f'Range:{range_list[i]} Mood:{mood_list[i]}\nDifficulty:{difficulty_list[i]} {duet_group_list[i]}')
+        sorted_list = []
+        year = ''
+        range = ''
+        mood = ''
+
+        match self.slider_Years.sliderPosition():
+            case 0:
+                year = '60s'
+            case 1:
+                year = '70s'
+            case 2:
+                year = '80s'
+            case 3:
+                year = '90s'
+            case 4:
+                year = '2000s'
+            case 5:
+                year = '2010s'
+            case _:
+                print("None")
+        print(year)
+
+        match self.slider_Range.sliderPosition():
+            case 0:
+                range = 'Bass'
+            case 1:
+                range = 'Baritone'
+            case 2:
+                range = 'Tenor'
+            case 3:
+                range = 'Alto'
+            case 4:
+                range = 'Mezzo'
+            case 5:
+                range = 'Soprano'
+            case _:
+                print("None")
+        print(range)
+
+        match self.slider_Mood.sliderPosition():
+            case 0:
+                mood = 'Bare your soul'
+            case 1:
+                mood = 'Swagger'
+            case 2:
+                mood = 'Get happy'
+            case 3:
+                mood = 'Just dance'
+            case 4:
+                mood = 'Rage on'
+            case _:
+                print("Unsure")
+        print(mood)
+
+
+
+#         f'"{title_list[i]} by {artist_list[i]} ({year_list[i]})\nGenre:{genre_list[i]} Time Period:{time_period_list[i]}\n'
+#         f'Range:{range_list[i]} Mood:{mood_list[i]}\nDifficulty:{difficulty_list[i]} {duet_group_list[i]}')
